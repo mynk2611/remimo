73dd5e6939c2bbce3f6d808be8158cc88ea23537
mynk2611 <mehta26mayank@gmail.com>
2024-07-05 12:38:56 +0530

    update button

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 
 export const Button = ({ children, onClick }: ButtonProps) => {
   return (
-    <button onClick={onClick} type="button" className="text-black bg-cyan-400 hover:bg-cyan-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1 me-2 mb-2 ">
+    <button onClick={onClick} type="button" className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-cyan-400 border border-black  text-black rounded-lg font-bold transform hover:-translate-y-0.5 transition duration-400">
       {children}
     </button>
   );
