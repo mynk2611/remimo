8ae886539ce8b9e56ef38689ec89675c2ab1c7c4
mynk2611 <mehta26mayank@gmail.com>
2024-07-18 16:54:10 +0530

    add status card classification

--- packages/ui/src/bankCard.tsx
+++ packages/ui/src/bankCard.tsx
@@ -1,12 +1,36 @@
 import StatusCard from "./statusCard";
 import { onrampStatus } from "@prisma/client";
 
+const statusColor = {
+        success : {
+                color : "lime-400",
+                gradientFrom : "from-lime-400",
+                gradientTo : "to-lime-500"
+        },
+        pending : {
+                color : "blue-400",
+                gradientFrom : "from-blue-400",
+                gradientTo : "to-blue-500"
+        },
+        failure : {
+                color : "red-400",
+                gradientFrom : "from-red-400",
+                gradientTo : "to-red-500"
+        
+        }
+}
+
+
 export function BankCard({transactions}:{transactions:{
         amount: number,
         status: onrampStatus,
         date: Date,
         provider: string
 }}){
+
+    const {status} = transactions;
+    const {color , gradientFrom , gradientTo} = statusColor[status];
+
     return(
     <div className="grid grid-cols-4 p-2 text-white hover:bg-zinc-800  ">
         <div className="flex flex-col justify-center ">
@@ -16,23 +40,23 @@ export function BankCard({transactions}:{transactions:{
         </div>
 
         <div className="flex flex-col justify-center ">
-                <div className="flex ">
+                <div className="flex justify-end">
                      $ {transactions.amount}
                 </div>
         </div>
 
         <div className="flex flex-col justify-center ">
-                <div className="flex ">
+                <div className="flex justify-end">
                       {transactions.provider}
                 </div>
         </div>
 
-        <div className="flex justify-center">
+        <div className="flex justify-end">
                 <StatusCard
                     status={transactions.status}
-                    color="lime-400"
-                    gradientFrom="from-lime-400"
-                    gradientTo="to-lime-500"
+                    color= {color}
+                    gradientFrom={gradientFrom}
+                    gradientTo={gradientTo}
                 />
         </div>
     </div>
