54dc665d1a3ed511826481dc4d65372ce84f85e3
mynk2611 <mehta26mayank@gmail.com>
2024-07-20 19:08:13 +0530

    improved card UI

--- packages/ui/src/phoneCard.tsx
+++ packages/ui/src/phoneCard.tsx
@@ -1,5 +1,7 @@
 import { transactionType, transactionStatus } from "@prisma/client";
 import StatusCard from "./statusCard";
+import { CreditIcon } from "./creditIcon";
+import { DebitIcon } from "./debitIcon";
 
 interface TransactionProps {
     amount: number;
@@ -12,37 +14,46 @@ interface TransactionProps {
 
 export function PhoneCard({ transactions }: { transactions: TransactionProps }) {
     return (
-        <div className="grid grid-cols-4 p-2 text-white hover:bg-zinc-800  ">
+        <div className="grid grid-cols-4 p-2 text-white hover:bg-zinc-800 border-b border-zinc-600 pt-3">
             <div className="flex ">
                 <div className="flex flex-col  ">
-                    <div className="text-xl">
-                        {transactions.type === 'Credit' ? 'Received' : 'Sent'}
+                    <div className="flex">
+                        <div className="text-xl">
+                            {transactions.type === 'Credit' ? 'Credit' : 'Debit'}
+                        </div>
+
+                        <div className="ml-2 mt-1">
+                            {transactions.type === 'Credit' ? <CreditIcon/> : <DebitIcon/>}
+                        </div>
                     </div>
-                    <div>
+
+                    <div className="text-xs">
                         {new Date(transactions.date).toLocaleDateString()}
                     </div>
                 </div>
             </div>
 
             <div className="flex flex-col justify-center ">
-                <div className="flex ">
+                <div className="flex justify-center">
                     {transactions.type === 'Credit' ? transactions.fromUserName : transactions.toUserName}
                 </div>
             </div>
 
             <div className="flex flex-col justify-center ">
-                <div className="flex ">
+                <div className="flex justify-center">
                     $ {transactions.amount}
                 </div>
             </div>
 
-            <div className="flex justify-center">
-                <StatusCard
-                    status={transactions.status}
-                    color="lime-400"
-                    gradientFrom="from-lime-400"
-                    gradientTo="to-lime-500"
-                />
+            <div className="flex flex-col justify-center">
+                <div className="flex justify-center">
+                    <StatusCard
+                        status={transactions.status}
+                        color="lime-400"
+                        gradientFrom="from-lime-400"
+                        gradientTo="to-lime-500"
+                    />
+                </div>
             </div>
         </div>
     );
