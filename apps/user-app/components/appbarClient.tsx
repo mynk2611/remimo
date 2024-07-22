7195f680b281161f937bda7a1986570ee392c46a
mynk2611 <mehta26mayank@gmail.com>
2024-07-22 18:12:07 +0530

    add scroll effect

--- apps/user-app/components/appbarClient.tsx
+++ apps/user-app/components/appbarClient.tsx
@@ -2,13 +2,30 @@
 import { signOut, useSession } from "next-auth/react"
 import { useRouter } from "next/navigation";
 import { Appbar } from "@repo/ui/appbar"
+import { useEffect, useState } from "react";
 
 export const AppbarClient = () => {
     const session = useSession();
     const route = useRouter();
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(()=>{
+        const handleScroll = ()=>{
+            if(window.scrollY > 50 ){
+                setIsScrolled(true)
+            }else{
+                setIsScrolled(false)
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll)
+        return ()=> {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    },[])
 
     return(
-    <div className="w-full">
+    <div className={` transition-all duration-300 ${isScrolled ? 'w-9/12' : 'w-full'}`}>
         <Appbar onSignout={async()=>{
             await signOut({redirect : false});
             route.push("/")
