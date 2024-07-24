c43806d81489a4ceace176427849de4d74389aa9
mynk2611 <mehta26mayank@gmail.com>
2024-07-25 03:16:00 +0530

    fix appbar scroll effect

--- apps/user-app/app/(dashboard pages)/p2p/page.tsx
+++ apps/user-app/app/(dashboard pages)/p2p/page.tsx
@@ -1,16 +1,12 @@
+import { DashboardAppbarClient } from "../../../components/DashboardAppbarClient";
 import { PayMoney } from "../../../components/PayMoney";
-import { AppbarClient } from "../../../components/appbarClient";
 
 export default function P2P() {
   return (
     <div className="spotlight-bg h-max">
-      <div className="flex justify-center">
-        <div className="w-10/12 flex justify-center mt-3">
-          <AppbarClient />
-        </div>
-      </div>
+      <DashboardAppbarClient/>
       <div className="flex justify-center h-min">
-        <div className="border rounded-2xl border-zinc-600 p-10 mt-6 mx-4 w-11/12">
+        <div className="border rounded-2xl border-zinc-600 p-10 mt-24 mx-4 w-11/12">
           <PayMoney />
         </div>
       </div>
--- apps/user-app/app/(dashboard pages)/transfer/page.tsx
+++ apps/user-app/app/(dashboard pages)/transfer/page.tsx
@@ -1,16 +1,12 @@
 import { AddMoney } from "../../../components/AddMoney";
-import { AppbarClient } from "../../../components/appbarClient";
+import { DashboardAppbarClient } from "../../../components/DashboardAppbarClient";
 
 export default function Transfer() {
   return (
     <div className="spotlight-bg h-max">
-      <div className="flex justify-center">
-        <div className="w-10/12 flex justify-center mt-3">
-          <AppbarClient />
-        </div>
-      </div>
+      <DashboardAppbarClient/>
       <div className="flex justify-center h-min">
-        <div className="border rounded-2xl border-zinc-600 p-10 mt-6 mx-4 w-11/12">
+        <div className="border rounded-2xl border-zinc-600 p-10 mt-24 mx-4 w-11/12">
           <AddMoney />
         </div>
       </div>
