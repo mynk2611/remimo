1b1a019000ab27d9cee83452063e593fd2fb0440
mynk2611 <mehta26mayank@gmail.com>
2024-07-26 13:27:04 +0530

    include props for card carousel functionality

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -3,6 +3,8 @@
 import { motion } from 'framer-motion';
 
 interface CardProps {
+  key ?: number ;
+  style ?: any;
   title: string;
   value: string;
   dateRange?: string;
@@ -15,23 +17,26 @@ interface CardProps {
   mt : string
 }
 
-const Card: React.FC<CardProps> = ({ title, value, dateRange, color, gradientFrom, gradientTo, width ="w-64", height = "h-36", icon, mt }) => {
+const Card: React.FC<CardProps> = ({ key, style, title, value, dateRange, color, gradientFrom, gradientTo, width ="w-64", height = "h-36", icon, mt }) => {
   return (
     <motion.div
-      className={`relative ${width} ${height} rounded-lg shadow-lg p-4 bg-gradient-to-r ${gradientFrom} ${gradientTo} text-white overflow-hidden`}
+      style={style}
+      className={`${ style ? "absolute" : "relative"} ${width} ${height} rounded-lg shadow-lg p-4 bg-gradient-to-r ${gradientFrom} ${gradientTo} ${style ? "border" : ""} text-white overflow-hidden`}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      whileHover={{
+      whileHover={style ? {} : {
         y: -10,
         boxShadow: "0 15px 30px rgba(0, 0, 0, 0.2)",
         transition: { duration: 0.2 },
       }}
     >
       {/* Spotlight effect */}
+      { style ? null : 
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="spotlight"></div>
-      </div>
+      <div className="spotlight"></div>
+    </div>
+    }
       <div className="relative z-10">
         <div className="flex justify-between items-center mb-2">
           <h2 className="text-base font-bold">{title}</h2>
