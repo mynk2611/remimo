42247c225bc7feac9d017d9747c870f900fbccb7
mynk2611 <mehta26mayank@gmail.com>
2024-08-22 16:14:02 +0530

    make component responsive

--- apps/user-app/components/FeatureSectionThree.tsx
+++ apps/user-app/components/FeatureSectionThree.tsx
@@ -17,17 +17,17 @@ export function FeatureSectionThree() {
         }}
         className="mt-1 bg-gradient-to-br  py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
-        <div className='grid grid-cols-2 pt-20 bg-gradient-to-b  relative'>
+        <div className='grid grid-cols-2 pt-20 pb-20 bg-gradient-to-b  relative'>
           <div className='flex justify-center items-center'>
                 <Coin/>
           </div>
 
           <div className='flex flex-col justify-center w-9/12 relative'>
-                <div className='text-5xl text-white font-bold'>
+                <div className='text-xl sm:text-5xl text-white font-extrabold sm:font-bold'>
                     Get ready for exciting new feature on Remimo!
                 </div>
 
-                <div className='text-lg text-white font- pt-5'>
+                <div className='text-sm sm:text-lg text-white font- pt-5'>
                 Soon, you'll be able to earn Remimo Points on your everyday spend, unlocking incredible rewards like travel perks, exclusive shopping discounts, and more. Stay tuned for the details!
                 </div>
           </div>
