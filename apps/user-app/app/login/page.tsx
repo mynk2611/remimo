a4b04b30946308e1ff2dbdb2b0b0a398f72c8da5
mynk2611 <mehta26mayank@gmail.com>
2024-09-05 18:15:05 +0530

    adjust outer div to improve layout responsiveness

--- apps/user-app/app/login/page.tsx
+++ apps/user-app/app/login/page.tsx
@@ -31,74 +31,77 @@ export default function Signup() {
   };
 
   return (
-    <div className="flex justify-center h-screen bg-black spotlight-bg">
-      <div className="relative border border-zinc-600 my-8 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black dark:bg-black">
-        <div className="flex justify-center">
-          <div>
-            <h2 className="font-bold text-xl text-neutral-200 dark:text-neutral-200">
-              Welcome Back to Remimo
-            </h2>
-          </div>
-        </div>
-        <div className="flex justify-center">
-          <div>
-            <h2 className="font-bold text-sm text-neutral-200 dark:text-neutral-200">
-              Create an account? <a href="/signup" className="text-cyan-500 dark:text-cyan-500">Signup</a>
-            </h2>
-          </div>
-        </div>
-
-        <div className="my-8">
-          <LabelInputContainer className="mb-5">
-            <div className="flex">
-              <Label htmlFor="phone">Phone Number</Label>
-              <div className="text-red-600 ml-1">*</div>
+    <div className="flex justify-center bg-black spotlight-bg">
+      <div className="flex flex-col min-h-screen overflow-auto justify-center w-full ">
+        <div className="mt-10 relative border border-zinc-600 my-8 w-10/12 sm:w-6/12 mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black dark:bg-black">
+          <div className="flex justify-center">
+            <div>
+              <h2 className="font-bold text-xl text-neutral-200 dark:text-neutral-200">
+                Welcome Back to Remimo
+              </h2>
             </div>
-            <Input id="phone" placeholder="84324XXXXX" type="text" onChange={(e) => setPhoneNumber(e.target.value)} />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-5">
-            <div className="flex">
-              <Label htmlFor="password">Password</Label>
-              <div className="text-red-600 ml-1">*</div>
+          </div>
+          <div className="flex justify-center">
+            <div>
+              <h2 className="font-bold text-sm text-neutral-200 dark:text-neutral-200">
+                Create an account? <a href="/signup" className="text-cyan-500 dark:text-cyan-500">Signup</a>
+              </h2>
             </div>
-            <Input id="password" placeholder="••••••••" type="password" onChange={(e) => setPassword(e.target.value)} />
-          </LabelInputContainer>
-          {error && <p className="text-red-600">{error}</p>} {/* Added error message display */}
-          <button
-            className="bg-gradient-to-br bg-zinc-800 relative group/btn from-zinc-900 dark:from-zinc-900 dark:to-zinc-900 to-zinc-900 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            onClick={handleLogin}
-          >
-            Log in &rarr;
-            <BottomGradient />
-          </button>
+          </div>
 
-          <div className="bg-gradient-to-r from-transparent via-neutral-700 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
-          <div className="flex flex-col space-y-4">
+          <div className="my-8">
+            <LabelInputContainer className="mb-5">
+              <div className="flex">
+                <Label htmlFor="phone">Phone Number</Label>
+                <div className="text-red-600 ml-1">*</div>
+              </div>
+              <Input id="phone" placeholder="84324XXXXX" type="text" onChange={(e) => setPhoneNumber(e.target.value)} />
+            </LabelInputContainer>
+            <LabelInputContainer className="mb-5">
+              <div className="flex">
+                <Label htmlFor="password">Password</Label>
+                <div className="text-red-600 ml-1">*</div>
+              </div>
+              <Input id="password" placeholder="••••••••" type="password" onChange={(e) => setPassword(e.target.value)} />
+            </LabelInputContainer>
+            {error && <p className="text-red-600">{error}</p>} {/* Added error message display */}
             <button
-              className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-              type="button" 
-              onClick={async()=> {
-                await signIn("github")
-              }}
+              className="bg-gradient-to-br bg-zinc-800 relative group/btn from-zinc-900 dark:from-zinc-900 dark:to-zinc-900 to-zinc-900 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+              onClick={handleLogin}
             >
-              <IconBrandGithub className="h-4 w-4 text-neutral-300 dark:text-neutral-300" />
-              <span className="text-neutral-300 dark:text-neutral-300 text-sm">GitHub</span>
+              Log in &rarr;
               <BottomGradient />
             </button>
-            <button
-              className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-              type="button" 
-              onClick={async()=>{
-                await signIn("google");
-              }}
-            >
-              <IconBrandGoogle className="h-4 w-4 text-neutral-300 dark:text-neutral-300" />
-              <span className="text-neutral-300 dark:text-neutral-300 text-sm">Google</span>
-              <BottomGradient />
-            </button>
+
+            <div className="bg-gradient-to-r from-transparent via-neutral-700 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+            <div className="flex flex-col space-y-4">
+              <button
+                className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                type="button"
+                onClick={async () => {
+                  await signIn("github")
+                }}
+              >
+                <IconBrandGithub className="h-4 w-4 text-neutral-300 dark:text-neutral-300" />
+                <span className="text-neutral-300 dark:text-neutral-300 text-sm">GitHub</span>
+                <BottomGradient />
+              </button>
+              <button
+                className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-zinc-900 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                type="button"
+                onClick={async () => {
+                  await signIn("google");
+                }}
+              >
+                <IconBrandGoogle className="h-4 w-4 text-neutral-300 dark:text-neutral-300" />
+                <span className="text-neutral-300 dark:text-neutral-300 text-sm">Google</span>
+                <BottomGradient />
+              </button>
+            </div>
           </div>
         </div>
+
       </div>
     </div>
   );
--- apps/user-app/app/signup/page.tsx
+++ apps/user-app/app/signup/page.tsx
@@ -24,10 +24,10 @@ export default function Signup() {
         password
       });
       router.push("/login");
-    }catch(error:any){
-      if (error.response && error.response.status === 400){
+    } catch (error: any) {
+      if (error.response && error.response.status === 400) {
         setError("Phone number already exists");
-      }else{
+      } else {
         console.log("Error while creating user account", error);
         setError("Something went wrong. Please try again later");
       }
@@ -35,69 +35,72 @@ export default function Signup() {
   }
 
   return (
-    <div className="flex justify-center h-min bg-black spotlight-bg">
-      <div className="relative border border-zinc-600 my-8 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black dark:bg-black">
-        <div className="flex justify-center">
-          <div className="">
-            <h2 className="font-bold text-xl text-neutral-200 dark:text-neutral-200">
-              Join Remimo
-            </h2>
+    <div className="flex justify-center bg-black spotlight-bg">
+      <div className="flex flex-col min-h-screen overflow-auto justify-center w-full">
+        <div className="relative border border-zinc-600 my-8 max-w-md w-10/12 sm:w-6/12 mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black dark:bg-black">
+          <div className="flex justify-center">
+            <div className="">
+              <h2 className="font-bold text-xl text-neutral-200 dark:text-neutral-200">
+                Join Remimo
+              </h2>
 
+            </div>
           </div>
-        </div>
-        <div className="flex justify-center">
-          <div className="">
-            <h2 className="font-bold text-sm text-neutral-200 dark:text-neutral-200">
-              Already have an account? <a href="/login" className="text-cyan-500 dark:text-cyan-500">Login</a>
-            </h2>
+          <div className="flex justify-center">
+            <div className="">
+              <h2 className="font-bold text-sm text-neutral-200 dark:text-neutral-200">
+                Already have an account? <a href="/login" className="text-cyan-500 dark:text-cyan-500">Login</a>
+              </h2>
 
+            </div>
           </div>
-        </div>
 
 
-        <div className="my-8" >
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="firstname">Name</Label>
-              <Input id="firstname" placeholder="Tyler" type="text" onChange={(e) => setName(e.target.value)} />
+          <div className="my-8" >
+            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+              <LabelInputContainer>
+                <Label htmlFor="firstname">Name</Label>
+                <Input id="firstname" placeholder="Tyler" type="text" onChange={(e) => setName(e.target.value)} />
+              </LabelInputContainer>
+            </div>
+            <LabelInputContainer className="mb-5">
+              <Label htmlFor="email">Email Address</Label>
+              <Input id="email" placeholder="projectmayhem@fc.com" type="email" onChange={(e) => setEmail(e.target.value)} />
             </LabelInputContainer>
+            <LabelInputContainer className="mb-5">
+              <div className="flex">
+                <Label htmlFor="phoneNumber">Phone Number</Label>
+                <div className=" text-red-600 ml-1">
+                  *
+                </div>
+              </div>
+              <Input id="phoneNumber" placeholder="84324XXXXX" type="email" onChange={(e) => setPhoneNumber(e.target.value)} />
+            </LabelInputContainer>
+            <LabelInputContainer className="mb-5">
+              <div className="flex">
+                <Label htmlFor="password">Password</Label>
+                <div className="text-red-600 ml-1">
+                  *
+                </div>
+              </div>
+              <Input id="password" placeholder="••••••••" type="password" onChange={(e) => setPassword(e.target.value)} />
+            </LabelInputContainer>
+            {error && <div className="text-red-600">{error}</div>}
+            <button
+              className="bg-gradient-to-br bg-zinc-800 relative group/btn from-zinc-900 dark:from-zinc-900 dark:to-zinc-900 to-zinc-900 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+              type="submit"
+              onClick={handleSignup}
+            >
+              Sign up &rarr;
+              <BottomGradient />
+            </button>
+
+            <div className="bg-gradient-to-r from-transparent via-neutral-700 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
           </div>
-          <LabelInputContainer className="mb-5">
-            <Label htmlFor="email">Email Address</Label>
-            <Input id="email" placeholder="projectmayhem@fc.com" type="email" onChange={(e) => setEmail(e.target.value)} />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-5">
-            <div className="flex">
-              <Label htmlFor="phoneNumber">Phone Number</Label>
-              <div className=" text-red-600 ml-1">
-                *
-              </div>
-            </div>
-            <Input id="phoneNumber" placeholder="84324XXXXX" type="email" onChange={(e) => setPhoneNumber(e.target.value)} />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-5">
-            <div className="flex">
-              <Label htmlFor="password">Password</Label>
-              <div className="text-red-600 ml-1">
-                *
-              </div>
-            </div>
-            <Input id="password" placeholder="••••••••" type="password" onChange={(e) => setPassword(e.target.value)} />
-          </LabelInputContainer>
-          {error && <div className="text-red-600">{error}</div>}
-          <button
-            className="bg-gradient-to-br bg-zinc-800 relative group/btn from-zinc-900 dark:from-zinc-900 dark:to-zinc-900 to-zinc-900 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            type="submit"
-            onClick={handleSignup}
-          >
-            Sign up &rarr;
-            <BottomGradient />
-          </button>
-
-          <div className="bg-gradient-to-r from-transparent via-neutral-700 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
         </div>
-      </div>
 
+
+      </div>
     </div>
   );
 }
