abdf5e14cb536a494e2645cfac1e1afd6fe2b88f
mynk2611 <mehta26mayank@gmail.com>
2024-09-14 13:05:51 +0530

    update file

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -7,49 +7,49 @@ const app = express();
 app.use(express.json());
 
 app.post("/hdfcWebhook", async (req, res) => {
-    const paymentInfo : {
-        token : string,
-        amount : string,
-        userId : string,
-        status : transactionStatus
-    } = {
-        token : req.body.token,
-        amount : req.body.amount,   
-        userId : req.body.user_Identifier,
-        status : req.body.status
-    }
+  const paymentInfo: {
+    token: string,
+    amount: string,
+    userId: string,
+    status: transactionStatus
+  } = {
+    token: req.body.token,
+    amount: req.body.amount,
+    userId: req.body.user_Identifier,
+    status: req.body.status
+  }
 
-    try {
-        await prisma.$transaction([
-            prisma.balance.updateMany({
-                where : {
-                    userId : Number(paymentInfo.userId)
-                },
-                data : {
-                    amount : {
-                        increment : Number(paymentInfo.amount)
-                    }
-                }
-            }),
-            prisma.onRamp.updateMany({
-                where : {
-                    token : paymentInfo.token
-                },
-                data : {
-                    status : paymentInfo.status
-                }
-            })         
-        ]);
-        res.json({
-            message : "Captured"
-        })
-        
-    } catch(e){
-        console.error(e);
-        res.status(411).json({
-            message : "error while processing webhook"
-        })
-    }
+  try {
+    await prisma.$transaction([
+      prisma.balance.updateMany({
+        where: {
+          userId: Number(paymentInfo.userId)
+        },
+        data: {
+          amount: {
+            increment: Number(paymentInfo.amount)
+          }
+        }
+      }),
+      prisma.onRamp.updateMany({
+        where: {
+          token: paymentInfo.token
+        },
+        data: {
+          status: paymentInfo.status
+        }
+      })
+    ]);
+    res.json({
+      message: "Captured"
+    })
+
+  } catch (e) {
+    console.error(e);
+    res.status(411).json({
+      message: "error while processing webhook"
+    })
+  }
 })
 
 app.listen(3003);
--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -6,68 +6,69 @@ import bcrypt from "bcryptjs";
 
 
 export const authOptions = {
-    providers: [
-      CredentialsProvider({
-          name: 'Credentials',
-          credentials: {
-            phone: { label: "Phone number", type: "text", placeholder: "1231231231" },
-            password: { label: "Password", type: "password" }
-          },
-          // TODO: User credentials type from next-aut
-          async authorize(credentials: any) {
-            if (!credentials) {
-                return null ;
-            }
-
-            const { phone , password } = credentials;
-
-            if( !phone || !password ) {
-                return null;
-            }
-
-            // Do zod validation, OTP validation here
-            const existingUser = await db.user.findFirst({
-                where: {
-                    number: phone,
-                }
-            });
-
-            if (existingUser) {
-                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
-                if (passwordValidation) {
-                    return {
-                        id: existingUser.id.toString(),
-                        name: existingUser.name,
-                        email: existingUser.number
-                    }
-                }
-                return null;
-            }
-            return null
-          },
-        }),
-        
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-        }),
-
-        GithubProvider({
-            clientId: process.env.GITHUB_ID || "",
-            clientSecret: process.env.GITHUB_SECRET || "",
-        })
-    ],
-    secret: process.env.JWT_SECRET || "secret",
-    callbacks: {
-        // TODO: can u fix the type here? Using any is bad
-        async session({ token, session }: any) {
-            session.user.id = token.sub
-
-            return session
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        phone: { label: "Phone number", type: "text", placeholder: "1231231231" },
+        password: { label: "Password", type: "password" }
+      },
+      // TODO: User credentials type from next-aut
+      async authorize(credentials: any) {
+        if (!credentials) {
+          return null;
         }
-    },
-    pages : {
-        signIn : '/login'
+
+        const { phone, password } = credentials;
+
+        if (!phone || !password) {
+          return null;
+        }
+
+        // Do zod validation, OTP validation here
+        const existingUser = await db.user.findFirst({
+          where: {
+            number: phone,
+          }
+        });
+
+        if (existingUser) {
+          console.log("hi there")
+          const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+          if (passwordValidation) {
+            return {
+              id: existingUser.id.toString(),
+              name: existingUser.name,
+              email: existingUser.number
+            }
+          }
+          return null;
+        }
+        return null
+      },
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    })
+  ],
+  secret: process.env.JWT_SECRET || "secret",
+  callbacks: {
+    // TODO: can u fix the type here? Using any is bad
+    async session({ token, session }: any) {
+      session.user.id = token.sub
+
+      return session
     }
+  },
+  pages: {
+    signIn: '/login'
   }
- 
+}
+
