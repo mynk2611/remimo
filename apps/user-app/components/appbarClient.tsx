d9ea71bc56eb4fb45af50a8e214c312a647a8590
mynk2611 <mehta26mayank@gmail.com>
2024-09-18 23:34:36 +0530

    add router refresh method

--- apps/user-app/components/appbarClient.tsx
+++ apps/user-app/components/appbarClient.tsx
@@ -6,37 +6,38 @@ import { useEffect, useState } from "react";
 import { Navbar } from "@repo/ui/navbar";
 
 export const AppbarClient = () => {
-    const session = useSession();
-    const route = useRouter();
-    const [isScrolled, setIsScrolled] = useState(false)
+  const session = useSession();
+  const route = useRouter();
+  const [isScrolled, setIsScrolled] = useState(false)
 
-    useEffect(()=>{
-        const handleScroll = ()=>{
-            if(window.scrollY > 50 ){
-                setIsScrolled(true)
-            }else{
-                setIsScrolled(false)
-            }
-        }
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
 
-        window.addEventListener("scroll", handleScroll)
-        return ()=> {
-            window.removeEventListener("scroll", handleScroll)
-        }
-    },[])
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
 
-    return(
+  return (
     <div className={` transition-all duration-300 ${isScrolled ? 'w-full sm:w-9/12' : 'w-full'}`}>
-        <Appbar onSignout={async()=>{
-            await signOut({redirect : false});
-            route.push("/")
-        }} user={session?.data?.user}
-           onSignin={async()=>{
-            await signIn();
-           }}>
+      <Appbar onSignout={async () => {
+        await signOut({ redirect: false });
+        route.push("/")
+        route.refresh()
+      }} user={session?.data?.user}
+        onSignin={async () => {
+          await signIn();
+        }}>
 
-            {session?.data?.user ? <Navbar/> : null}
-        </Appbar>
+        {session?.data?.user ? <Navbar /> : null}
+      </Appbar>
     </div>
-    )
+  )
 }
