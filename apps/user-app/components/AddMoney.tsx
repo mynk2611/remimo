4dec2b14a4e27772951432477a2a884c9f87510f
mynk2611 <mehta26mayank@gmail.com>
2024-09-28 11:59:32 +0530

    fix boxes UI for phone view

--- apps/user-app/components/AddMoney.tsx
+++ apps/user-app/components/AddMoney.tsx
@@ -26,7 +26,9 @@ export function AddMoney() {
   return (
     <div className="relative w-full h-screen overflow-hidden bg-zinc-800 flex flex-col items-center justify-center rounded-lg">
       <div className="absolute inset-0 w-full h-full bg-zinc-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-      <Boxes />
+      <div className="hidden md:block">
+        <Boxes />
+      </div>
 
       <div className={`relative w-10/12 md:w-7/12 lg:w-5/12 rounded-lg shadow-lg p-4 bg-black text-white overflow-hidden z-40`} >
         <MoneyTransferCard title="Add Money">
